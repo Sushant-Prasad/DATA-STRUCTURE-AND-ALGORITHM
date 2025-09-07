a93818576addd4ec965619e27baa9214ec0f4188
Sushant-Prasad <sushantprasad77@gmail.com>
2025-09-07 20:33:16 +0530

    Comment updated

--- JAVA/Hashing/HashMap3.java
+++ JAVA/Hashing/HashMap3.java
@@ -51,6 +51,11 @@ public class HashMap3 {
 
     // Step 1: Count frequency of each element
     for (int i = 0; i < arr.length; i++) {
+      // if(map.containsKey(arr[i])){
+      //   map.put(arr[i], map.get(arr[i])+1);
+      // }else{
+      //   map.put(arr[i], 1);
+      // }
       map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
     }
 
